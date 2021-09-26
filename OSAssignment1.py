eb6d509e97fc0a9108aaf20d3c69b3e8fd96d153
ASBP <s194582@student.dtu.dk>
2021-09-26 20:16:21 +0200

    Martin - Kommentarer

--- OSAssignment1.py
+++ OSAssignment1.py
@@ -5,15 +5,16 @@ print("Test")
 
 #def Command(x):
 def Command(x):
-    #print("Virker cd commanden?")
     
+    #initialisation of the fork system call. 
     rc = os.fork()
+    #if rc is equal to 0 the creation of the child process succeeded. 
     if rc == 0:
         print("Child process (pid", os.getpid(),")")
-        os.execvp(x[0], x)
+        os.execvp(x[0], x) #this terminates the child process and returns to the parent process.
         
     if rc > 0:
-        os.wait()
+        os.wait() #wait call so the child process finishes before returning to the parent process. 
         print("Parent process (pid:", os.getpid,")")
     
 def cd(x):
@@ -23,16 +24,16 @@ if __name__ == "__main__":
     
     while 1:
         print("TEST2")
-        #x = input("$ ").split()
-        x = input("$ ").split()
-        if x[0] == "exit":
+        x = input("$ ").split() #takes the user input and splits the string between each space
+        #space 0 is the system command and space 1 is the target
+        
+        #terminates the terminal.
+        if x[0] == "exit": 
             break
+        #cd system call for changing the directory x[1] is the target directory.    
         if x[0] == "cd":
-            #cd(x)
             cd(x[1])
         else:
+        #we use command for all the systems calls that needs a child process i.e ls, cat, pwd.
             Command(x)
-    #run()
-
-
-
+    
