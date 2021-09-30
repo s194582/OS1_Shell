630b1934bcaae51446b6b4c104cdfd3b3764384d
SAA-DK <70898210+SAA-DK@users.noreply.github.com>
2021-09-30 12:08:35 +0000

    Det virker! Mangler oprydning + commentarer
    LORTET FUCKING VIRKER!!!!!!!

--- OSAssignment1.py
+++ OSAssignment1.py
@@ -1,56 +1,91 @@
 import os
 
-
+STDIN = 0
+STDOUT = 1 #dette er for så den ikke smider den ud til skærmen ved exec() funktionen
 print("Test")
+
 
 #def Command(x):
 def Command(x):
-    
+
     #initialisation of the fork system call. 
     rc = os.fork()
     #if rc is equal to 0 the creation of the child process succeeded. 
     if rc == 0:
         print("Child process (pid", os.getpid(),")")
         os.execvp(x[0], x) #this terminates the child process and returns to the parent process.
-        
+
     if rc > 0:
         os.wait() #wait call so the child process finishes before returning to the parent process. 
-        print("Parent process (pid:", os.getpid,")")
-    
+        print("Parent process (pid:", os.getpid(),")")
+
 def cd(x):
     os.chdir(x)
 
+def pipe(x):
+    #File descriptors r,w for Reading and Writing
+    y = x[0].split()
+    z = x[1].split()
+    r, w = os.pipe()
+    rc1 = os.fork()
+
+    #Check if fork() has been run
+    if rc1 < 0:
+        print("OSError")
+
+    if rc1 == 0:
+        #child
+        print("TEST!!!!!")
+
+        print("Child process (pid", os.getpid(),")")
+       
+        rc3 = os.fork()
+        if rc3 == 0:
+            #child
+            os.close(r)
+            os.dup2(w, STDOUT)
+            exit()
+            
+            
+        else:
+            #parent
+            os.close(w)
+            os.dup2(r, STDIN) #kopirer processen og sætter den ene ende af pipen istedet for STDOUT
+                
+            os.wait()
+            os.execvp(z[0], z)
+    else:
+        #parents
+        os.wait()
+
+
+
 if __name__ == "__main__":
-    
+
     while 1:
-        print("TEST2")
-        x = input("$ ").split() #takes the user input and splits the string between each space
-        #space 0 is the system command and space 1 is the target
-        
+        i = 0
+        x = input("$ ")
+
+        if "|" in x:
+            i = 1
+            pipe(x.split("|"))
+
+
         #terminates the terminal.
         if x[0] == "exit": 
             break
         #cd system call for changing the directory x[1] is the target directory.    
         if x[0] == "cd":
             cd(x[1])
-        for m in x:
-            if m == "|":
-                r, w = os.pipe()
-                rc2 = os.fork()
-                if rc2:
-                    os.close(w)
-                    r = os.fdopen(r)
-                    print("This is parent process, write something to for the child process to read")
-                    str = r.read()
-                    print("text ", str)
-                else:
-                    os.close(r)
-                    w = os.fdopen(w, 'w')
-                    print("child writing ", w)
-                    w.write("Written by child:")
-                    w.close()
 
-        else:
-        #we use command for all the systems calls that needs a child process i.e ls, cat, pwd.
-            Command(x)
+        else:    
+            if i == 0:
+                y = x.split() #takes the user input and splits the string between each space
+                #space 0 is the system command and space 1 is the target
+                Command(y) 
+
+
+
+
+
     
