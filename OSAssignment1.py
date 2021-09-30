164dc0d6529f8c4f48f933e7ea8e6f39633a743f
SAA-DK <70898210+SAA-DK@users.noreply.github.com>
2021-09-30 20:17:01 +0000

    Alt virker nu, og er klar til aflevering

--- OSAssignment1.py
+++ OSAssignment1.py
@@ -1,4 +1,3 @@
-
 import os
 
 STDIN = 0
@@ -7,6 +6,10 @@ STDOUT = 1 #dette er for så den ikke smider den ud til skærmen ved exec() funk
 def Command(x):
     #create a child process
     rc = os.fork()
+
+    if rc < 0:
+        print(OSError)
+
     #if rc is equal to 0 the child process is running.
     if rc == 0:
         #execute a new progam, replacing the current process. when done running the process, terminate the process
@@ -27,44 +30,32 @@ def cd(x):
 
 
 def pipe(x):
-    #The string x is split, and defined as two new string()
-    z = x[1].split()
+    #The string x is split at the "|" symbol, and defined as two new string()
+    a = x.split("|")
     #File descriptors r,w for Reading and Writing
-    r, w = os.pipe()
-    #create a child process
-    rc1 = os.fork()
-
-    #Check if fork() has been run
-    if rc1 < 0:
-        print("OSError")
-
-    if rc1 == 0:
-        #child
-        #create a new child process inside the earlier child process.
-        #By doing this, the old child process will be both a child process, but also a parent process.
-        rc3 = os.fork()
-        if rc3 == 0:
-            #child
-            os.close(r)
-            os.dup2(w, STDOUT)
-            #close the child process
-            exit()
-            
-            
-        else:
-            #parent
-            os.close(w)
-            os.dup2(r, STDIN) #kopirer processen og sætter den ene ende af pipen istedet for STDOUT
-                
-            os.wait() #Stop the parent process, until the child process is done.
-            #execute a new progam, replacing the current process. when done running the process, terminate the process
-            os.execvp(z[0], z)
+    if os.fork():
+    #parent
+        os.wait() #Stop the parent process, until the child process is done.
     else:
-        #parent
-        os.wait() #Stop the parent process, until the child process is done. 
+        #child, and also parent of child2
+        w,r = os.pipe() #initialising the pipe
+        if os.fork():
+            # Child of parent, and parent of child2
+            # Closes a file descriptor, so that it no longer refers to any file,
+            # and may be reused
+            os.close(r)
+            os.dup2(w, STDIN) #Copies the file descriptor w, into STDIN 
+            str = a[1]
+            str = str.replace(' ', '') #this helps with user input errors.
+            os.execlp(str,'-1') #this executes the right side of the pipe of the command 
+        else:
+            #child2: child of child1
+            os.close(w)    #Closes a file descriptor, so that it no longer refers to any file, and may be reused
+            os.dup2(r, STDOUT) #Copies the file descriptor r, into STDOUT
+            os.execvp('ps', a[0].split()) #Splits the commands of the left side of the pipe and executes the command
+            #instead of the execvp command goes into STDOUT it is redirected into the right side of the command.
 
-
-
+ 
 if __name__ == "__main__":
 
     while 1:
@@ -80,8 +71,7 @@ if __name__ == "__main__":
             #Give variable i the value 1
             i = 1
             #Run the defined function pipe(), 
-            #while also splitting the user input in two, where "|" is in the user input
-            pipe(x.split("|"))
+            pipe(x)
 
 
         #Check if user input is equal to "exit"
